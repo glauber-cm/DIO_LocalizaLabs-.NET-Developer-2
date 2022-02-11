435d1c87b9fa50090a2406a494c0b673efc46244
glauber-cm <54870054+glauber-cm@users.noreply.github.com>
2022-02-11 19:37:26 -0300

    Desafios C#

---
+++ Desafios C#/04-Xenlonguinho/04-Xenlonguinho/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04_Xenlonguinho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numero; i++)
+            {
+                int esferas = int.Parse(Console.ReadLine());
+                Console.WriteLine(esferas - Math.Floor(Math.Sqrt(esferas)));
+            }
+        }
+    }
+}
---
+++ Desafios C#/05 - PedroBentoMundodeOZ/5 - PedroBentoMundodeOZ/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5___PedroBentoMundodeOZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> joiasDistinct = new List<string>();   
+            string strJoias = " ";
+
+            strJoias = Console.ReadLine().Trim();
+
+            while (strJoias != "")
+            {
+                if (strJoias.Contains("(") || strJoias.Contains(")"))
+                {
+                    joiasDistinct.Add(strJoias);
+                }
+
+                strJoias = Console.ReadLine();
+
+                if (strJoias == null)
+                {
+                    strJoias = "";
+                }
+            }
+            Console.WriteLine(joiasDistinct.Distinct().Count());
+
+
+        }
+
+    }
+
+
+}
+
