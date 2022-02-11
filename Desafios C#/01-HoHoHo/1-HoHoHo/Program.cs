785c040522baf1daf70cb955ae01ca3a22bd5179
glauber-cm <54870054+glauber-cm@users.noreply.github.com>
2022-02-11 19:34:46 -0300

    Desafios C#

---
+++ Desafios C#/01-HoHoHo/1-HoHoHo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1_HoHoHo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            //Exibir "Ho" do papai noel
+            for (int i = 0; i < N; i++)
+            {
+                if (i < N - 1)
+                {
+                    Console.Write("Ho ");                  //Complete o código no espaço em branco
+                }
+                else
+                {
+                    Console.WriteLine("Ho!");
+                }
+            }
+        }
+    }
+}
---
+++ Desafios C#/03-Esfera/03-Esfera/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03_Esfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi, raio, volume;
+            pi = 3.14159;
+            raio = double.Parse(Console.ReadLine());
+
+            volume = (4.0 * pi * (Math.Pow(raio, 3.0))/3.0);
+
+            Console.WriteLine("VOLUME = " + Math.Round(volume, 3).ToString("F3"));
+
+        }
+    }
+}
